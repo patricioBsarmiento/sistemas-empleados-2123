71297f4f732fa6d7365335874aa453d7c1af917d
pato <patriciobelinsarmiento@gmail.com>
2021-07-26 15:41:02 -0300

    ya casi hecho

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
+from flask import send_from_directory
 from flaskext.mysql import MySQL
+from datetime import datetime
+import os
+
+from pymysql.cursors import Cursor
 
 app = Flask(__name__)
 #MODIFICAN LO QUE SEA 
@@ -12,38 +17,108 @@ app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'sistema2123'
 mysql.init_app(app)
 
+CARPETA = os.path.join('uploads')
+app.config['CARPETA'] = CARPETA
 
+@app.route('/uploads/<nombreFoto>')
+def uploads(nombreFoto):
+   return send_from_directory(app.config['CARPETA'], nombreFoto)
 
 
 @app.route('/')
 def index():
-    sql = "INSERT INTO `empleado` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'messi', 'messi@ciudad.com.ar', 'messi.jpg');"
+    sql = "SELECT * FROM `empleado`;"
     conn = mysql.connect()
     cursor = conn.cursor()
     cursor.execute(sql)
     conn.commit()
+    empleados = cursor.fetchall()
     
-    return render_template('empleados/index.html')
-    
+    return render_template('empleados/index.html', empleados = empleados)
+
+
+
+@app.route('/destroy/<int:id>')
+def destroy(id):
+   conn = mysql.connect()
+   cursor = conn.cursor()
+
+   cursor.execute("SELECT foto FROM empleado WHERE id=%s", id)
+   fila = cursor.fetchall()
+   os.remove(os.path.join(app.config['CARPETA'],fila[0][0]))
+   
+   cursor.execute("DELETE FROM empleado WHERE id=%s", (id))
+   conn.commit()
+   return redirect('/')
+
+
+@app.route('/edit/<int:id>')
+def edit(id):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM empleado WHERE id=%s", (id))
+    empleados = cursor.fetchall()
+    conn.commit()
+    return render_template('empleados/edit.html', empleados = empleados)
+
+
+@app.route('/update', methods=['POST'])
+def update():
+    _nombre = request.form['txtNombre']
+    _correo = request.form['txtCorreo']
+    _foto = request.files['txtFoto']
+    id = request.form['txtID']
+
+    sql = "UPDATE empleado SET nombre=%s, correo=%s WHERE id=%s;"
+    datos = (_nombre, _correo, id)
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    now = datetime.now()
+    tiempo = now.strftime('%Y%H%M%S')
+
+    if _foto.filename != '':
+        nuevoNombreFoto = tiempo+_foto.filename
+        _foto.save("uploads/" + nuevoNombreFoto)
+
+        cursor.execute("SELECT foto FROM empleado WHERE id=%s", id)
+        fila = cursor.fetchall()
+        #print(os.path.join(app.config[0][0]))
+        os.remove(os.path.join(app.config['CARPETA'],fila[0][0]))
+        cursor.execute("UPDATE empleado SET foto=%s WHERE id=%s", (nuevoNombreFoto, id))
+        conn.commit()
+
+    cursor.execute(sql, datos)
+    conn.commit()
+    return redirect('/')
+
 @app.route('/create')
 def create():
-   return render_template('empleados/create.html')
+    return render_template('empleados/create.html')
 
 
 @app.route('/store', methods=['POST'])
 def storage():
-   _nombre = request.form['txtNombre']
-   _correo = request.form['txtCorreo']
-   _foto = request.files['txtFoto']
+    _nombre = request.form['txtNombre']
+    _correo = request.form['txtCorreo']
+    _foto = request.files['txtFoto']
 
-   sql = "INSERT INTO `empleado` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
-   datos = (_nombre, _correo, _foto.filename)
+    now = datetime.now()
+    tiempo = now.strftime("%Y%H%M%S")
 
-   conn = mysql.connect()
-   cursor = conn.cursor()
-   cursor.execute(sql, datos)
-   conn.commit()
-   return render_template('empleados/index.html')
+    if _foto.filename != '':
+        nuevoNombreFoto = tiempo + _foto.filename
+        _foto.save("uploads/" + nuevoNombreFoto) 
+
+
+    sql = "INSERT INTO `empleado` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
+    datos = (_nombre, _correo, nuevoNombreFoto)
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()
+    return redirect('/')
 
 
 
